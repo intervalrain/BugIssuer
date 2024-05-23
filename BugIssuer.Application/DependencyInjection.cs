b8603174f1e2dcb3995395a19ab7cc92a39c210b
Rain Hu <intervalrain@gmail.com>
2024-05-24 02:24:51 +0800

    buildinfrastructure

---
+++ BugIssuer.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using BugIssuer.Application.Common.Behaviors;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugIssuer.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            // AuthorizationBehavior
+            options.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        return services;
+    }
+}
+
---
+++ BugIssuer.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,53 @@
+﻿using BugIssuer.Application.Common.Interfaces;
+using BugIssuer.Infrastructure.Common.Persistance;
+using BugIssuer.Infrastructure.Common.Services;
+using BugIssuer.Infrastructure.Issuer.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace BugIssuer.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddHttpContextAccessor()
+            .AddServices()
+            .AddPersistence();
+            //.AddBackgroundServices(configuration)
+
+        return services;       
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IDateTimeProvider, SystemDateTimeProvider>();
+        return services;
+    }
+
+    private static IServiceCollection AddPersistence(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(""));
+
+        services.AddScoped<IIssueRepository, IssueRepository>();
+
+        return services;
+    }
+
+    //private static IServiceCollection AddBackgroundService(this IServiceCollection services, IConfiguration configuration)
+    //{
+    //    services.AddEmailNotifications(configuration);
+    //    return services;
+    //}
+
+    //private static IServiceCollection AddEmailNotifications(this IServiceCollection services, IConfiguration configuration)
+    //{
+
+    //}
+}
+
